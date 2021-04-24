f3c6514f1971aa16f4962c70a51e79e1cbc44dc3
Christian Dinglasan <cmdinglasan@student.apc.edu.ph>
2021-04-25 00:05:14 +0800

    Initial Commit

---
+++ resources/js/Layouts/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="min-h-screen bg-gray-50">
+        <div class="fixed z-20 top-0 left-0 h-8 w-full bg-white border-b">
+            <div class="px-4">
+                <a href="//arstlemedia.com" class="h-8 flex items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd" />
+                    </svg>
+                    <span class="text-xs uppercase font-bold">Return to Arstle Media</span>
+                </a>
+            </div>
+        </div>
+        <div class="relative pt-8 h-screen">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,56 @@
+<template>
+    <app-layout>
+        <div class="relative">
+            <div class="card-list" :class="screenSize == 'full' ? 'full' : screenSize == 'tablet' ? 'tablet' : 'mobile', cardDisplay ? 'shown' : 'not-shown' ">
+                <div class="relative h-screen overflow-auto">
+                    <div class="py-2 flex items-center transition" :class="cardDisplay ? 'justify-end h-14 px-4' : 'justify-center h-10'">
+                        <button class="h-2 w-8 rounded-full bg-gray-300" @click="toggleCardDisplay" v-if="!cardDisplay"></button>
+                        <button class="h-10 w-10 rounded-full" @click="toggleCardDisplay" v-else>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="map">
+                <iframe id="openstreet" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://umap.openstreetmap.fr/en/map/community-pantries-ph_598704"></iframe>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/App'
+
+export default {
+    name: "Home",
+    components: {
+        AppLayout,
+    },
+    data() {
+        return {
+            screenSize: 'full',
+            cardDisplay: false,
+        }
+    },
+    mounted() {
+      if(window.outerWidth <= 360) {
+          this.screenSize = 'mobile';
+      } else if(window.outerWidth > 360 && window.outerWidth <= 768) {
+          this.screenSize = 'tablet'
+      } else {
+          this.screenSize = 'full';
+      }
+    },
+    methods: {
+        toggleCardDisplay() {
+          this.cardDisplay = !this.cardDisplay;
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
