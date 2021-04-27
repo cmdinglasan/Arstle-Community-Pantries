bff1d9ceb0603b674e436756be1fd4029624603c
Christian Dinglasan <cmdinglasan@student.apc.edu.ph>
2021-04-27 16:40:15 +0800

    Added Register Link on Login
    Added Register Link on Login
    Hide other user info

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -28,6 +28,10 @@
                 Forgot your password?
             </inertia-link>
 
+            <inertia-link :href="route('register')" class="ml-4 underline text-sm text-gray-600 hover:text-gray-900">
+                Register
+            </inertia-link>
+
             <breeze-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                 Log in
             </breeze-button>
